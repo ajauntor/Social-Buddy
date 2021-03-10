3e002b7c978f6439dec1f7dcf889f7a81d8ca647
AJ Auntor <auntorknocks@gmail.com>
2021-03-10 22:27:55 +0600

    React route dom import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      
+      <h2>Hi This is AJ Auntor!</h2>
     </div>
     
   );
