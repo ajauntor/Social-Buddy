45b568f835a9e38bdf1ee53554a20a33d15000c7
AJ Auntor <auntorknocks@gmail.com>
2021-03-10 22:49:51 +0600

    Created Home and About Component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
-      
+       <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {
+
+        }
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
     
   );
